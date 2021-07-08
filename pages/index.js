9bbf1e951038fece8a014acbaa76b646a275c53a
Jamie Sinclair <jamie.sinclair@houzz.com>
2021-07-08 13:12:36 +0900

    add bug replication

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 export default function Page() {
     return (
         <div>
-            Index Page
+            <h1>Index Page</h1>
+            <ul>
+                <li><a href="/about/?テスト=あ">Link to location with query containing special chars that will throw error</a></li>
+            </ul>
         </div>
     )
 }
